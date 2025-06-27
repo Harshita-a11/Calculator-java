ee2ae6a062b2a8522eb3c262ee1c9121c2ee4860
Harshita-a11 <harshitaj1811@gmail.com>
2025-06-27 20:47:05 +0530

    Add B.java

--- B.java
+++ B.java
@@ -3,6 +3,20 @@ class B
 {
 public static void main(String...s)
 {
+	try
+{
+FileOutputStream ob=new FileOutputStream("F1.txt");
+String g="Java";
+byte b[]=g.getBytes();
+byte a[]={'a','b','c'};
+ob.write(a);
+ob.write(b);
+ob.close();
+}
+catch(Exception e)
+{
+System.out.println(e);
+}
 try
 {
 FileInputStream ob=new FileInputStream("F1.txt");
